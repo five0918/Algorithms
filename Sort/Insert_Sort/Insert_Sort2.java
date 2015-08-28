5144e0b185b9309f6bc308742d2b6949fafb2326
Excavator <five0918@163.com>
2015-08-28 19:45:00 +0800

    1 updated

---
+++ Sort/Insert_Sort/Insert_Sort2.java
@@ -0,0 +1,29 @@
+public class Insert_Sort2{
+
+	public static void insert_sort(int[] array){
+		int N=array.length;
+		for (int i=1;i<N;i++) {
+			int temp=array[i];
+			for (int j=i-1;j>=0;j--) {
+				if (temp<array[j]) {
+					array[j+1]=array[j];
+					array[j]=temp;
+				}
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] arr={3,44,38,5,47,15,36,26,27,2,46,4,19,50,48};
+		System.out.println("ÅÅÐòÇ°:");
+		for (int n :arr ) {
+			System.out.print(n+" ");
+		}
+		insert_sort(arr);
+		System.out.println("\nÅÅÐòºó:");
+		for (int n :arr ) {
+			System.out.print(n+" ");
+		}
+	}
+}
